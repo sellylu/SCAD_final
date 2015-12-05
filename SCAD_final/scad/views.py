a39c0f9d2524725035f33399c11163a03e5eec9b
Liu <liu@LiudeMacBook-Pro.local>
2015-12-05 16:43:41 +0800

    get user id finish

--- SCAD_final/scad/views.py
+++ SCAD_final/scad/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db import connection
 # Create your views here.
+
 def index(request):
+	if request.method == 'POST':	
+
+		id = request.POST['user_id']
+		print(id)
+		#insert_id_command = 'INSERT INTO "scad_post" ("id") VALUES ('+id +')' 
+		cursor = connection.cursor()
+		cursor.execute("INSERT INTO user(user_id) VALUES (%s)", [id])
+		#cursor.execute(insert_id_command)
+	
 	return render(request, 'index.html', {})
