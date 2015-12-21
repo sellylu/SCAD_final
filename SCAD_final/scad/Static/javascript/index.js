b8d78528618e2cc4ffc7f54f5fe91fa2b9a6770a
Liu <liu@LiudeMacBook-Pro.local>
2015-12-21 15:43:16 +0800

    fix wrong spell

--- SCAD_final/scad/Static/javascript/index.js
+++ SCAD_final/scad/Static/javascript/index.js
@@ -2,7 +2,35 @@ $.ajaxSetup({
     data: {csrfmiddlewaretoken: '{{ csrf_token }}' },
 });
 
+function closecal(){
+    $("#finished_time_date").attr('disabled','disabled');
+}
+
+function opencal(){
+$("#finished_time_date").removeAttr('disabled');
+}
+
 function creategroup_submit() {
+
+
+    check_group_name = $('#group_name').val();
+    check_group_intro = $('#intro').val();
+    nosubmit = 0;
+    if(check_group_intro ==''){
+
+            $('#introdiv').attr('class','form-group has-error');
+            nosubmit =1;
+    }else{
+            $('#introdiv').attr('class','form-group');
+    }
+    if(check_group_name==''){
+           $('#namediv').attr('class','form-group has-error');
+            nosubmit =1;
+    }else{
+
+            $('#namediv').attr('class','form-group');
+    }
+    if(nosubmit==1)return false;
 
     creator_id = Cookies.get('user_id');
 
@@ -10,16 +38,22 @@ function creategroup_submit() {
 
     intro = document.getElementById("intro").value;
 
-    if(document.getElementById("optionsRadios1").checked){
+    if(document.getElementById("private_op1").checked){
         private = 0;
     }else{
         private = 1;
     }
 
+    if(document.getElementById("finished_time_op1").checked){
+        finished_time = '';
+    }else{
+        finished_time = document.getElementById("finished_time_date").value;
+    }
+
     date = Date.now();
     group_id = creator_id + date;
     member_limit = parseInt(document.getElementsByName("member_limit")[0].value);
-    $.post( "/index/", { group_id : group_id, group_name : group_name,  member_limit :member_limit,intro:intro,private:private,creator_id:creator_id })
+    $.post( "/index/", { group_id : group_id, group_name : group_name,  member_limit :member_limit,intro:intro,private:private,creator_id:creator_id ,finished_time:finished_time})
         .then(function () {
             window.location = '/group/'+group_id;
             $("#create_group_Modal").modal('hide');
