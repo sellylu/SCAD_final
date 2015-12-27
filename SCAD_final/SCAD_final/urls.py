502945924dc2ade86b756d171a4ea85f62e5f172
Liu <liu@LiudeMacBook-Pro.local>
2015-12-27 22:23:27 +0800

    add file

--- SCAD_final/SCAD_final/urls.py
+++ SCAD_final/SCAD_final/urls.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 from scad.views import index
 from scad.views import group_page
 from scad.views import user_page
-from scad.views import group_member
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^index/$', index),
     url(r'^group/(?P<group_id>[0-9]+)',group_page),
     url(r'^user/(?P<user_id>[0-9]+)',user_page),
-    url(r'^user/(?P<user_id>[0-9]+)/group_member',group_member),
 
 ]
 
